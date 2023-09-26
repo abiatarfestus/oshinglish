7fa305aa5dcf634e51a5126d34f1910db4f832f5
abiatarfestus <40578707+abiatarfestus@users.noreply.github.com>
2023-09-26 22:28:07 +0200

    Update serializers

--- oshinglish/api/dictionary/rest_views.py
+++ oshinglish/api/dictionary/rest_views.py
@@ -1,10 +1,22 @@
 from rest_framework import permissions, viewsets
 
-from .models import EnglishWord, OshindongaPhonetic, OshindongaWord
+from oshinglish.apps.dictionary.models import (
+    EnglishWord,
+    OshindongaPhonetic,
+    WordPair,
+    WordPairDefinition,
+    DefinitionExample,
+    PartOfSpeech,
+    OshindongaIdiom
+)
 from .serializers import (
     EnglishWordSerializer,
     OshindongaPhoneticSerializer,
-    OshindongaWordSerializer,
+    WordPairSerializer,
+    WordPairDefinitionSerializer,
+    DefinitionExampleSerializer,
+    PartOfSpeechSerializer,
+    OshindongaIdiomSerializer
 )
 
 
@@ -18,13 +30,13 @@ class EnglishWordViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 
-class OshindongaWordViewSet(viewsets.ModelViewSet):
+class WordPairViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows Oshindonga words to be viewed or edited.
     """
 
-    queryset = OshindongaWord.objects.all().order_by("word")
-    serializer_class = OshindongaWordSerializer
+    queryset = WordPair.objects.all().order_by("word")
+    serializer_class = WordPairSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
@@ -35,4 +47,44 @@ class OshindongaPhoneticViewSet(viewsets.ModelViewSet):
 
     queryset = OshindongaPhonetic.objects.all().order_by("oshindonga_word")
     serializer_class = OshindongaPhoneticSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class WordPairDefinitionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows word pairdefinition to be viewed or edited.
+    """
+
+    queryset = WordPairDefinition.objects.all().order_by("word_pair__english_word__word")
+    serializer_class = WordPairDefinitionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class DefinitionExampleViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows word definition example to be viewed or edited.
+    """
+
+    queryset = DefinitionExample.objects.all().order_by("id")
+    serializer_class = WordPairDefinitionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PartOfSpeechViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows part of speech to be viewed or edited.
+    """
+
+    queryset = PartOfSpeech.objects.all().order_by("code")
+    serializer_class = PartOfSpeechSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class OshindongaIdiomViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Oshindonga idiom to be viewed or edited.
+    """
+
+    queryset = OshindongaIdiom.objects.all().order_by("word_pair__oshindonga_word")
+    serializer_class = OshindongaIdiomSerializer
     permission_classes = [permissions.IsAuthenticated]
--- oshinglish/api/dictionary/serializers.py
+++ oshinglish/api/dictionary/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import EnglishWord, OshindongaPhonetic, OshindongaWord
+from oshinglish.apps.dictionary.models import EnglishWord, OshindongaPhonetic, WordPair, WordPairDefinition, DefinitionExample, PartOfSpeech, OshindongaIdiom
 
 
 class EnglishWordSerializer(serializers.HyperlinkedModelSerializer):
@@ -9,13 +9,37 @@ class EnglishWordSerializer(serializers.HyperlinkedModelSerializer):
         fields = ["url", "id", "word", "word_case"]
 
 
-class OshindongaWordSerializer(serializers.HyperlinkedModelSerializer):
+class WordPairSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = OshindongaWord
-        fields = ["url", "id", "english_word", "word", "word_case", "word_phonetics"]
+        model = WordPair
+        fields = ["url", "id", "english_word", "oshindonga_word", "root", "part_of_speech", "synonyms"]
 
 
 class OshindongaPhoneticSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = OshindongaPhonetic
-        fields = ["url", "id", "oshindonga_word", "pronunciation", "phonetics"]
+        fields = ["url", "id", "word_pair", "oshindonga_phonetics", "oshindonga_pronunciation"]
+
+
+class OshindongaIdiomSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = OshindongaIdiom
+        fields = ["url", "id", "word_pair", "oshindonga_idiom", "meaning"]
+
+
+class WordPairDefinitionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = WordPairDefinition
+        fields = ["url", "id", "word_pair", "english_definition", "oshindonga_definition"]
+
+
+class DefinitionExampleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = DefinitionExample
+        fields = ["url", "id", "definition", "english_example", "oshindonga_example"]
+
+
+class PartOfSpeechSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = PartOfSpeech
+        fields = ["url", "id", "code", "english_name", "oshindonga_name", "example"]
